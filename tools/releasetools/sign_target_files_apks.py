7622b8fc10728f0e0832a195125a1f5cb24136fe
DivestOS <tavi@divested.dev>
2024-08-22 09:05:03 +0000

    uncatched DOS scripted change(s)
    Change-Id: I892e4de0c0f00f11f97bd04740bcfa5c86a6dfb3

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -946,8 +946,9 @@ def ReplaceOtaKeys(input_tf_zip, output_tf_zip, misc_info):
       raise common.ExternalError(k + " does not exist or is not a file")
 
   mapped_keys = []
-  for k in keylist:
-    m = re.match(r"^(.*)\.x509\.pem$", k)
+  for kl in keylist:
+    k = kl.decode('ISO-8859-1')
+    m = re.match(r"^(.*)$", k)
     if not m:
       raise common.ExternalError(
           "can't parse \"%s\" from META/otakeys.txt" % (k,))
