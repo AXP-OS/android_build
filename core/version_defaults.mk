4afa1915d57dc4d909c29deb00fd097749c571f2
DivestOS <tavi@divested.dev>
2024-09-30 19:33:06 +0000

    uncatched DOS scripted change(s)
    Change-Id: I5ed813a5c67f0f8538458a0b4aa259b351681d42

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -250,7 +250,7 @@ ifndef PLATFORM_SECURITY_PATCH
     #  It must be of the form "YYYY-MM-DD" on production devices.
     #  It must match one of the Android Security Patch Level strings of the Public Security Bulletins.
     #  If there is no $PLATFORM_SECURITY_PATCH set, keep it empty.
-      PLATFORM_SECURITY_PATCH := 2023-02-05
+      PLATFORM_SECURITY_PATCH := 2024-09-05
 endif
 .KATI_READONLY := PLATFORM_SECURITY_PATCH
 
@@ -321,6 +321,6 @@ ifndef PLATFORM_MIN_SUPPORTED_TARGET_SDK_VERSION
   # Used to set minimum supported target sdk version. Apps targeting sdk
   # version lower than the set value will result in a warning being shown
   # when any activity from the app is started.
-  PLATFORM_MIN_SUPPORTED_TARGET_SDK_VERSION := 23
+  PLATFORM_MIN_SUPPORTED_TARGET_SDK_VERSION := 28
 endif
 .KATI_READONLY := PLATFORM_MIN_SUPPORTED_TARGET_SDK_VERSION
